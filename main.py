02347b71817f678a735248961bc974ebf3da80c0
OliviafortuneUCD <Test@test.com>
2021-11-11 18:51:43 +0000

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ covid = pd.read_csv("COVID.csv")
 
 # Choose entries with county Name of Cork.
 Corkonly  = covid[covid['CountyName'] == 'Cork']
-
+Dublin  = covid[covid['CountyName'] == 'Dublin']
 
 
 print(Corkonly.to_string())
